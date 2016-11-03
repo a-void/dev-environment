476d1b609fc87721d4afe8e72b622f67e7edbe4e
a-void <avitiuk@me.com>
2016-11-02 21:22:52 -0400

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack');
+
+module.exports = {
+  debug : true,
+  devtool : 'eval',
+  entry: './src/app/app.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ]
+};
