0b6e22333a27601cd0ce1309e80a565e3e383978
a-void <avitiuk@me.com>
2017-02-13 21:29:27 -0500

    Improve gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
-var webpackConfig = require('./webpack.config.js');
 
 // config
 var config = {
@@ -9,10 +8,28 @@ var config = {
   dist: 'dist/',
 };
 
+// environment config
+var isProduction  = process.env.NODE_ENV === 'production';
+var webpackConfig = require('./webpack.config.js');
+
+// https://github.com/ai/autoprefixer
+var autoprefixerBrowsers = [
+  'ie >= 9',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 6',
+  'opera >= 23',
+  'ios >= 6',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+// combine js files into webpack
 gulp.task('js', function() {
   return gulp.src(webpackConfig.entry)
     .pipe($.webpack(webpackConfig))
-    .pipe(gulp.dest(config.dist))
+    .pipe(gulp.dest(`${config.dist}/scripts/`))
     .pipe($.connect.reload());
 });
 
@@ -25,20 +42,30 @@ gulp.task('html', function() {
 
 // copy images
 gulp.task('img', function(cb) {
-  return gulp.src(`${config.src}/public/img/**/*.{png,jpg,jpeg,gif}`)
-    .pipe(gulp.dest(`${config.dist}/img/`));
+  return gulp.src(`${config.src}/img/**/*.{png,jpg,jpeg,gif}`)
+    .pipe(gulp.dest(`${config.dist}/images/`));
+});
+
+// copy fonts
+gulp.task('font', function(cb) {
+  return gulp.src(`${config.src}/fonts/*`)
+    .pipe(gulp.dest(`${config.dist}/fonts/`));
 });
 
 // convert stylus to css
 gulp.task('styles',function() {
-  return gulp.src(`${config.src}/styles/app.styl`)
-    .pipe($.stylus())
-    .pipe(gulp.dest(`${config.dist}/css/`))
+  return gulp.src(`${config.src}/app.styl`)
+    .pipe($.stylus({
+      // only compress during production
+      compress: isProduction
+    }))
+    .pipe($.autoprefixer({browsers: autoprefixerBrowsers}))
+    .pipe(gulp.dest(`${config.dist}/styles/`))
     .pipe($.connect.reload());
 });
 
-// waits until is finished then builds the project
-gulp.task('build', ['styles', 'js', 'img', 'js'], function() {
+// build the project after centrain tasks are finished
+gulp.task('build', ['styles', 'js', 'img', 'font'], function() {
   return gulp.src(`${config.src}/index.html`)
     .pipe(gulp.dest(config.dist))
     .pipe($.connect.reload());
@@ -58,11 +85,11 @@ gulp.task('server:connect', ['build'], function() {
 // watch styl, html and js file changes
 gulp.task('server', ['server:connect'], function() {
   gulp.watch(`${config.src}/styles/*.styl`, ['styles']);
-  gulp.watch(`${config.src}/index.html`, ['html']);
-  gulp.watch(`${config.src}/app/app.js`, ['js']);
+  gulp.watch(`${config.src}/**/*.html`, ['js']);
+  gulp.watch(`${config.src}/**/*.js`, ['js']);
 });
 
-// by default build project and then watch files in order to trigger livereload
+// build project by default and then watch files in order to trigger livereload
 gulp.task('default', function() {
   gulp.start('server');
 });
