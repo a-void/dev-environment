b308e6fee7e5d2a0c0cf2584423175509b086bb8
a-void <avitiuk@me.com>
2017-02-13 21:31:01 -0500

    Improve webpack config and build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
+var path = require('path');
 var webpack = require('webpack');
 
+var APP_PATH = __dirname
+var BUILD_PATH = 'build'
+var PUBLIC_PATH = '/'
+
 module.exports = {
-  debug : true,
-  devtool : 'eval',
-  entry: './src/app/app.js',
+  debug: !!process.env.NODE_ENV === 'production',
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval',
+  entry: './src/app.js',
   output: {
-    path: __dirname,
-    filename: 'bundle.js'
+    path: path.join(APP_PATH, BUILD_PATH),
+    publicPath: PUBLIC_PATH,
+    filename: 'bundle.js',
   },
-  plugins: [
+  module: {
+    loaders: [
+      {
+        test: /.json$/,
+        loaders: ['json']
+      }
+    ]
+  },
+  plugins: process.env.NODE_ENV === 'production' ? [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new webpack.optimize.DedupePlugin(0),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
     })
   ]
 };
